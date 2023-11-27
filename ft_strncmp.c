c158cefabc033ae1edda11793b7bcc744014e5f7
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-27 14:32:48 +0100

    update

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,24 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/27 11:40:35 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/27 14:32:20 by mduran-l         ###   ########.fr       */
+/*   Created: 2023/11/27 14:16:12 by mduran-l          #+#    #+#             */
+/*   Updated: 2023/11/27 14:32:31 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+include "libft.h"
 
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *b, int c, size_t len);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen(const char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strrchr(const char *s, int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && n)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i ++;
+		n ++;
+	}
+	return (0);
+}
