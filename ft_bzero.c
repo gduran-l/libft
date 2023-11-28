30c08ec4b6a758046d8bc09c6b258e699ac51da6
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 14:38:44 +0100

    update

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,20 +6,22 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:50:14 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/27 15:02:43 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:38:10 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-void	ft_bzero(char *s, size_t n)
+/*
+	The bzero() function writes n zeroed bytes to the string s.
+	If n is zero, bzero() does nothing.
+*/
+void	ft_bzero(void *s, size_t n)
 {
-	if (!n)
-		return ;
-	while (n)
+	size_t	i;
+
+	i = 0;
+	while (i < n)
 	{
-		*s = '0';
-		s ++;
-		n --;
+		((char *)s)[i ++] = 0;
 	}
-	*s = '\0';
 }
