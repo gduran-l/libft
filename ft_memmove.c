09f41af3afeee74d1596bba157dc22143f4c2771
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 14:55:22 +0100

    update

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,12 +6,24 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:10:36 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/28 13:22:04 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:49:33 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	The memmove() function copies len bytes from string src to string dst.
+	The two strings may overlap; the copy is always done in a non-destructive
+	manner.
+
+	The memmove() function returns the original value of dst.
+*/
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	return (dst);
+	void	*out;
+
+	out = dst;
+	while (len --)
+		((char *)dst)[len] = ((char *)src)[len];
+	return (out);
 }
