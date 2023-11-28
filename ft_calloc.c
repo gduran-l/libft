d0abd712aa18b2432d2ffa4c6958a46e1778434c
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 13:40:45 +0100

    ft_calloc.c

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/28 13:17:39 by mduran-l          #+#    #+#             */
+/*   Updated: 2023/11/28 13:36:09 by mduran-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+/*
+	The calloc() function contiguously allocates enough space for count objects
+	that are size bytes of memory each and returns a pointer to the allocated
+	memory. The allocated memory is filled with bytes of value zero.
+
+	If successful, calloc() function returns a pointer to allocated memory.
+	If there is an error, they return a NULL pointer and set errno to ENOMEM.
+*/
+void	*calloc(size_t count, size_t size)
+{
+	void	*output;
+
+	output = malloc(count * size);
+	return (output);
+}
