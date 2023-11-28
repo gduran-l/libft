0decf051b4b54120f7e04cf8b857a11cf54655da
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 12:40:56 +0100

    ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,22 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 12:11:06 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/28 12:12:13 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/11/28 12:27:27 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*memcpy(void *dst, const void *src, size_t n)
 {
+	int	i;
+
+	i = 0;
+	while (n)
+	{
+		dst = (void *)src;
+		src ++;
+		dst ++;
+		n --;
+	}
 	return (dst);
 }
