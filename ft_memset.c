e03297690064023d35ebe5ac0368cab48e8ca75e
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 14:54:54 +0100

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,20 +6,22 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 13:47:23 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/27 15:25:33 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/11/28 14:53:56 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	The memset() function writes len bytes of value c (converted to an
+	unsigned char) to the string b.
+
+	The memset() function returns its first argument.
+*/
 void	*ft_memset(void *b, int c, size_t len)
 {
-	int	i;
-
-	i = 0;
 	while (len --)
 	{
-		*(char *)b = (char)c;
-		b ++;
+		((char *)b)[len] = (char)c;
 	}
 	return (b);
 }
