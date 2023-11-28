9a972bbda6fadd49529a46dca810cf6be5c434ce
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-28 13:57:25 +0100

    update

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,19 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 11:25:31 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/27 11:47:11 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/11/28 13:52:02 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	The isalpha() function tests for any character for which isupper(3) or
+	islower(3) is true. The value of the argument must be representable as
+	an unsigned char or the value of EOF.
+
+	The isalpha() function returns zero if the character tests false and
+	returns non-zero if the character tests true.
+*/
 int	ft_isalpha(int c)
 {
 	if (c < 65 || (c > 90 && c < 97) || c > 122)
