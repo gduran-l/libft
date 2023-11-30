fb97bf417655a9b39559ad6096153a9bf155f1b0
Gustavo Duran <mduran-l@student.42malaga.com>
2023-11-30 15:02:16 +0100

    bonus.h

---
+++ libft_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/30 14:58:57 by mduran-l          #+#    #+#             */
+/*   Updated: 2023/11/30 15:02:00 by mduran-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef LIBFT_BONUS_H
+# define LIBFT_BONUS_H
+# include <stdlib.h>
+# include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+#endif
