b3ea0b33b5c6dfebc72d323cad78219325bc527b
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-01 16:29:47 +0100

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,60 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:47:56 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/30 15:48:14 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:11:45 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+static size_t	ft_nbrlen(int n)
+{
+	size_t	len;
+
+	len = 0;
+	while (n)
+	{
+		n /= 10;
+		len ++;
+	}
+	if (n < 0)
+		len ++;
+	return (len);
+}
+
+/*
+	Utilizando malloc(3), genera una string que represente el valor entero
+	recibido como argumento. Los números negativos tienen que gestionarse.
+*/
 char	*ft_itoa(int n)
 {
-	return ("CACA");
+	size_t			l;
+	unsigned int	nbr;
+	char			*output;
+
+	l = ft_nbrlen(n);
+	output = (char *)malloc((l + 1) * sizeof(char));
+	nbr = (unsigned int)n;
+	if (n < 0)
+	{
+		output[0] = '-';
+		nbr = (unsigned int)-n;
+	}
+	output[l + 1] = '\0';
+	while (l)
+	{
+		output[l] = 48 + (nbr % 10);
+		nbr /= 10;
+		l --;
+	}
+	return (output);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s\n", ft_itoa(-5234));
+	printf("%s\n", ft_itoa(36234));
+	printf("%s\n", ft_itoa(2147483647));
+	printf("%s\n", ft_itoa(-2147483648));
+	return (0);
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,12 +6,36 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:48:29 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/30 15:48:49 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/01 16:28:56 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	A cada carácter de la string ’s’, aplica la función ’f’ dando como paráme-
+	tros el índice de cada carácter dentro de ’s’ y el propio carácter.
+	Genera 	una nueva string con el resultado del uso sucesivo de ’f’
+*/
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	return ("CACA");
+	char			*output;
+	char			*temp;
+	unsigned int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		temp = (*f)(i, s[i]);
+		l += ft_strlen(temp);
+		i ++;
+	}
+	output = (char *)malloc((l + i) * sizeof(char));
+	i = 0;
+	while (s[i])
+	{
+		temp = (*f)(i, s[i]);
+		ft_strlcat(output, temp, ft_strlen(temp));
+		i ++;
+	}
+	return (output);
 }
