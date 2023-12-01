c15ae1ec0b0cbb3f2553c6b86b8c285e5eb30680
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-01 15:24:46 +0100

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,44 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:47:19 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/30 15:47:40 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/01 15:24:14 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	Reserva (utilizando malloc(3)) un array de strings resultante de separar
+	la string ’s’ en substrings utilizando el caracter ’c’ como delimitador.
+	El array debe terminar con un puntero NULL.
+*/
 char	**ft_split(char const *s, char c)
 {
-	return ("CACA");
+	size_t	k;
+	size_t	i;
+	size_t	j;
+	char	**output;
+
+	k = 0;
+	i = 0;
+	j = 0;
+	output = (char **)malloc((ft_strlen(s) + 1) * sizeof(char));
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			output[j][k ++] = '\0';
+			j ++;
+			continue ;
+		}
+		output[j][k ++] = s[i];
+		i ++;
+	}
+	output[j + 1] = 0;
+	return (output);
+}
+
+int	main(void)
+{
+	ft_split("dabale arroz a la zorra el abad", ' ');
+	return (0);
 }
