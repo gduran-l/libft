a5a12d308d1c05e45de3379cdcfda61dbd6d9476
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-01 14:52:48 +0100

    ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,28 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:45:07 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/30 15:45:46 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/01 14:52:21 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	Reserva (con malloc(3)) y devuelve una substring de la string ’s’.
+	La substring empieza desde el índice ’start’ y tiene una longitud máxima
+	’len’.
+*/
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	return ("CACA");
+	unsigned int	i;
+	char			*output;
+
+	i = 0;
+	output = (char *)malloc(len + 1 * sizeof(char));
+	while ((size_t)i < len)
+	{
+		output[i] = s[start + i];
+		i ++;
+	}
+	output[i] = '\0';
+	return (output);
 }
