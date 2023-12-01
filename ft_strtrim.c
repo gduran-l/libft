87d7d6604aeff7f7c1c3d3d5c9946c4af37cf442
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-01 15:13:47 +0100

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,35 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:46:41 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/30 15:47:05 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/01 15:13:15 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	return ("CACA");
+	int		add;
+	size_t	c;
+	size_t	i;
+	size_t	j;
+	char	*output;
+
+	output = (char *)malloc(ft_strlen(s1) * sizeof(char));
+	i = 0;
+	c = 0;
+	while (s1[i])
+	{
+		j = 0;
+		add = 1;
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+				add = 0;
+			j ++;
+		}
+		if (add)
+			output[c ++] = s1[i];
+		i ++;
+	}
+	return (output);
 }
