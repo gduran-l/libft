e5f241b6512ca684ac589b64728199250f8f62c1
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-05 09:57:40 +0100

    update

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,19 +6,30 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 10:34:21 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/11/28 10:45:47 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/05 09:57:32 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
+/*
+	The memchr() function locates the first occurrence of c (converted to an
+	unsigned char) in string s.
+
+	The memchr() function returns a pointer to the byte located, or NULL if no
+	such byte exists within n bytes.
+*/
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	while ((char)s || n)
+	size_t	i;
+
+	i = 0;
+	while (((unsigned char *)s)[i])
 	{
-		if ((char)s == (char)c)
-			return ((void *)s);
-		n --;
-		s ++;
+		if (i == n)
+			break ;
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)&s[i]);
+		i ++;
 	}
 	return (0);
 }
