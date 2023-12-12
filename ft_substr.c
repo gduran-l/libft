8d2d3f03e646d1d06876026f4e3d1a52278dd67d
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-12 12:32:01 +0100

    perfect substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:45:07 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/12/05 14:38:30 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/12 12:30:56 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -22,16 +22,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	l;
 	char	*output;
 
-	i = 0;
 	l = ft_strlen(s);
-	if (len > l)
-		len = l;
-	output = (char *)malloc((len + 1) * sizeof(char));
+	if (start >= l)
+	{
+		start = l;
+		len = 0;
+	}
+	if (len > l - start)
+		len = l - start;
+	output = (char *)ft_calloc(len + 1, sizeof(char));
 	if (!output)
 		return (0);
-	ft_bzero(output, len + 1);
-	if (start > l)
-		return (output);
+	i = 0;
 	while (s[start + (unsigned int)i] && i < len)
 	{
 		output[i] = s[start + (unsigned int)i];
