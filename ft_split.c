59a99562e166537ab93ef88f29fe34fa1b4b3ec9
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-12 16:44:24 +0100

    split strict

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,12 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 15:47:19 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/12/12 12:14:36 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/12 16:43:24 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static size_t	ft_end(const char *str, size_t start, char c)
+static size_t	get_end(const char *str, size_t start, char c)
 {
 	while (str[start])
 	{
@@ -22,7 +22,7 @@ static size_t	ft_end(const char *str, size_t start, char c)
 	return (start);
 }
 
-static size_t	ft_start(const char *str, size_t start, char c)
+static size_t	get_start(const char *str, size_t start, char c)
 {
 	while (str[start])
 	{
@@ -33,16 +33,16 @@ static size_t	ft_start(const char *str, size_t start, char c)
 	return (start);
 }
 
-static size_t	ft_words(const char *s, char c)
+static size_t	count_words(const char *s, char c)
 {
 	size_t	count;
 	size_t	i;
 
 	count = 0;
-	i = ft_start(s, 0, c);
+	i = get_start(s, 0, c);
 	while (s[i])
 	{
-		i = ft_start(s, ft_end(s, i, c), c);
+		i = get_start(s, get_end(s, i, c), c);
 		count ++;
 	}
 	return (count);
@@ -57,22 +57,25 @@ char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	char	*temp;
 	char	**output;
 
-	j = 0;
-	output = (char **)malloc((ft_words(s, c) + 1) * sizeof(char *));
+	output = (char **)malloc((count_words(s, c) + 1) * sizeof(char *));
 	if (!output)
 		return (0);
-	i = ft_start(s, 0, c);
+	i = get_start(s, 0, c);
+	j = 0;
 	while (s[i])
 	{
-		temp = ft_substr(s, (unsigned int)i, ft_end(s, i, c) - i);
-		output[j] = ft_calloc(ft_strlen(temp) + 1, sizeof(char));
-		ft_memmove(output[j], temp, ft_strlen(temp));
-		free(temp);
+		output[j] = ft_substr(s, (unsigned int)i, get_end(s, i, c) - i);
+		if (!output[j])
+		{
+			while (j)
+				free(output[--j]);
+			free(output);
+			return (0);
+		}
 		j ++;
-		i = ft_start(s, ft_end(s, i, c), c);
+		i = get_start(s, get_end(s, i, c), c);
 	}
 	output[j] = 0;
 	return (output);
