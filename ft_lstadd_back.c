c69171772d4a4ea7ede6825939b1747e8f78f519
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-13 12:13:47 +0100

    update

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 09:44:27 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/12/13 10:21:32 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:13:11 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -16,8 +16,12 @@
 */
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*last;
-
-	last = ft_lstlast(&(*lst[0]));
-	last->next = new;
+	if (!lst || !new)
+		return ;
+	if (!(*lst))
+	{
+		*lst = new;
+		return ;
+	}
+	ft_lstlast(*lst)->next = new;
 }
