8c53f1da0bb703c040648d78b7c3e29b5d8f2ff4
Gustavo Duran <mduran-l@student.42malaga.com>
2023-12-13 12:54:44 +0100

    update

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 09:45:41 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/12/13 12:37:04 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/13 12:41:44 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -23,8 +23,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	if (!lst)
 		return ;
 	tmp = *lst;
-	(*del)(tmp->content);
-	if (tmp->next)
-		ft_lstclear(&tmp->next, del);
-	free(tmp);
+	ft_lstclear(&tmp->next, del);
+	ft_lstdelone(tmp, del);
 }
