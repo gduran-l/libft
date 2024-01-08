2699ab71f239dcf45f471ee075184b08b615a4c6
mduran-l <mduran-l@student.42malaga.com>
2024-01-08 11:25:27 +0100

    fix

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,16 +6,44 @@
 /*   By: mduran-l <mduran-l@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:34:17 by mduran-l          #+#    #+#             */
-/*   Updated: 2023/12/11 12:22:34 by mduran-l         ###   ########.fr       */
+/*   Updated: 2023/12/17 17:58:57 by mduran-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-static int	gofish(int c)
+static int	getbegin(const char *s, int *neg)
 {
-	if (c == 43 || c == 45 || (c >= 9 && c <= 13) || c == 32 || ft_isdigit(c))
-		return (0);
-	return (1);
+	int	c;
+
+	c = 0;
+	while (s[c])
+	{
+		if (ft_isdigit(s[c]))
+			return (c);
+		if (s[c] == 45)
+			*neg = -1;
+		if (!(s[c] > 8 && s[c] < 14) && s[c] != 32 && s[c] != 43
+			&& s[c] != 45)
+			return (-1);
+		if ((s[c] == 43 || s[c] == 45) && !ft_isdigit(s[c + 1]))
+			return (-1);
+		c ++;
+	}
+	return (c);
+}
+
+static int	getend(const char *s, int start)
+{
+	int	c;
+
+	c = start;
+	while (s[c])
+	{
+		if (!ft_isdigit(s[c]))
+			return (c);
+		c ++;
+	}
+	return (c);
 }
 
 /*
@@ -27,27 +55,21 @@ static int	gofish(int c)
 */
 int	ft_atoi(const char *str)
 {
-	int			n;
-	int			neg;
-	size_t		i;
+	int	n;
+	int	neg;
+	int	start;
+	int	i;
 
 	n = 0;
-	i = 0;
 	neg = 1;
-	while (str[i] != '\0')
+	start = getbegin(str, &neg);
+	if (start < 0)
+		return (0);
+	i = start;
+	while (i < getend(str, start))
 	{
-		if (((str[i] == 43 || str[i] == 45) && !ft_isdigit(str[i + 1])))
-			return (0);
-		if (gofish(str[i]) || (n && !ft_isdigit(str[i])) || (!n && str[i] == 48
-				&& !ft_isdigit(str[i + 1])))
-			return (n * neg);
-		if (str[i] == 45)
-			neg = -1;
-		if (ft_isdigit(str[i]))
-		{
-			n *= 10;
-			n += str[i] - 48;
-		}
+		n *= 10;
+		n += str[i] - 48;
 		i ++;
 	}
 	return (n * neg);
